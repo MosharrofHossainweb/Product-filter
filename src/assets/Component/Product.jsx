d082959399fb3cea9bc1130b5024c5c2072ffcd3
Mosharrof Hossain <mosharrofhossainpdl@gmail.com>
2024-10-28 16:24:52 +0600

    first commit

---
+++ src/assets/Component/Product.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import bddata from '../../Db/Db'
+const Product = () => {
+    
+// =========variable part=============
+const[data,setData]=useState(bddata)
+
+console.log(bddata)
+  return (
+    <>
+      <section className="product_filter mt-4 bg-slate-700">
+        <div className="container">
+            <div className="product_btn flex justify-around bg-yellow-700 p-4">
+                <button className='text-xl p-1 px-5 rounded-lg hover:bg-slate-400 '>All</button>
+                <button className='text-xl p-1 px-5 rounded-lg hover:bg-slate-400 '>Apple</button>
+                <button className='text-xl p-1 px-5 rounded-lg hover:bg-slate-400 '>Hp</button>
+                <button className='text-xl p-1 px-5 rounded-lg hover:bg-slate-400 '>Lenevo</button>
+                <button className='text-xl p-1 px-5 rounded-lg hover:bg-slate-400 '>Asus</button>
+            </div>
+            <div className="product_card flex mt-5 flex-wrap gap-5">
+                {
+                    data.map((item,i)=>(
+
+                <div key={i} className="singleCard w-[400px]  rounded-xl h-[280px] shadow-[-1px_1px_6px_4px_#fcdd2d] gap-4 flex mt-5 mb-10">
+                        <div className="card_img w-[200px] pt-9 h-full overflow-hidden bg-slate-300">
+                            <img src={item.image} alt="" />
+                        </div>
+                        <div className="card_text relative ">
+
+                            {
+                                item.discount&&
+                                <div className="sale  absolute bg-slate-500 px-3 py-1 text-[10px] text-yellow-200 rounded-lg top-1 right-1">
+                                    <h2 className='font-semibold'>Sale</h2>
+                                </div>
+                            }
+
+                            <h2 className='text-2xl text-yellow-400 mt-3 '>{item.brand}</h2>
+                            <p className='text-yellow-400 mt-2'>Model: {item.model_number}</p>
+                            <ul className='list-disc pl-5 mt-4'>
+                                <li className='text-yellow-400'>Ram: {item.ram}</li>
+                                <li className='text-yellow-400'>Rom: {item.rom}</li>
+                                <li className='text-yellow-400'>Display Size: {item.display_size}</li>
+                                <li className='text-yellow-400'>Color: {item.color}</li>
+                            </ul>
+                            <h2 className='text-yellow-500 mt-6 '>Price: {item.price_in_tk}tk 
+                                 <span className='text-green-50'><br />Discount Price: {item.discount_price}</span>tk </h2>
+                        </div>
+                </div>
+                    ))
+                }
+            </div>
+
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Product
